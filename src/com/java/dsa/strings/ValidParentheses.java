2b76dcadbf890ca61c6fcbc665223a694af83c14
Prabhakar <kulkarniprabhakar2000@gmail.com>
2024-02-01 10:20:47 +0530

    Validating parentheses using stack - solved.

---
+++ src/com/java/dsa/strings/ValidParentheses.java
@@ -0,0 +1,45 @@
+package com.java.dsa.strings;
+
+import java.util.Stack;
+
+public class ValidParentheses {
+
+	public static void main(String[] args) {
+
+		String s = "[()]";
+//		String s = "() {} []";
+//		String s = "[)";
+		System.out.println(validateParentheses(s));
+		
+	}
+	
+	public static boolean validateParentheses(String s) {
+		
+//		To validate this we can you a stack DS
+		
+		Stack<Character> stack = new Stack<>();
+		
+		for(int i=0; i<s.length(); i++) {
+			
+			char ch = s.charAt(i);
+			
+			if(ch == '(' || ch == '{' || ch == '[') {
+				
+				stack.push(ch);
+			}
+			else if(ch == ')' || ch == '}' || ch == ']') {
+				if(stack.isEmpty()) {
+					return false;
+				}
+				char cc = stack.pop();
+				
+				if((ch == '(' && cc != ')') || (ch == '{' && cc != '}') || (ch == '[' && cc != ']')) {
+					return false;
+				}
+			}
+		}
+		
+		return stack.isEmpty();
+	}
+
+}
