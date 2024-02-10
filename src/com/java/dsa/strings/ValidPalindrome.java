a5245e0e76f8403918f5c5c2f84e42a7e638bb20
Prabhakar <kulkarniprabhakar2000@gmail.com>
2024-02-10 23:14:43 +0530

    Updated code with dry run check.

--- src/com/java/dsa/strings/ValidPalindrome.java
+++ src/com/java/dsa/strings/ValidPalindrome.java
@@ -18,39 +18,41 @@ public class ValidPalindrome {
 
 	public static void main(String[] args) {
 
+		String str = "aba";
 		String str = "abca";
+		String str = "abaad";
 		
 		System.out.println(validatingPalindrome(str));
 		
 	}
 	
-	public static boolean validatingPalindrome(String str){
+	public static boolean validatingPalindrome(String str) {
 		
-		int left = 0;
-		int right = str.length()-1;
+		int l_pointer = 0; // starting from start
+		int r_pointer = str.length()-1; // starting from right both left & right pointers start point defined.
 		
-		while(left<right) {
+		while(l_pointer<= r_pointer) {
 			
-			if(str.charAt(left) != str.charAt(right)) {
-				
-				return isPalindrome(str, left + 1, right) || isPalindrome(str, left, right-1); 
+			if(str.charAt(l_pointer) != str.charAt(r_pointer)) {
+				return isPlaindrome(str, l_pointer+1, r_pointer) || isPlaindrome(str, l_pointer+1, r_pointer);
 			}
-			left ++;
-			right --;
+			l_pointer++;
+			r_pointer--;
 		}
-		return true;
 		
+		return true;
 	}
 	
-	public static boolean isPalindrome(String str, int left, int right) {
+	public static boolean isPlaindrome(String str, int left, int right) {
 		
-		while(left < right) {
+		while(left<=right) {
 			
-			if(str.charAt(left) != str.charAt(right)) {
+			if(str.charAt(left)!= str.charAt(right)) {
 				return false;
 			}
-			left ++;
-			right --;
+			
+			left++;
+			right--;
 		}
 		
 		return true;
