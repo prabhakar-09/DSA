cb4ea56dc998d72ad568f076f726c0e413cd6391
Prabhakar <kulkarniprabhakar2000@gmail.com>
2024-03-29 10:13:43 +0530

    Implementing add first, last & other methods in Linked lists.

--- src/com/java/dsa/linkedLists/z_practice/LinkedListImpl.java
+++ src/com/java/dsa/linkedLists/z_practice/LinkedListImpl.java
@@ -3,9 +3,11 @@ package com.java.dsa.linkedLists.z_practice;
 public class LinkedListImpl {
 
 	private static Node head;
+	private static Node tail;
 
 	public LinkedListImpl(){
 		this.head = null;
+		this.tail = null;
 	}
 
 	public class Node{
@@ -19,21 +21,31 @@ public class LinkedListImpl {
 	}
 	
 //	Adding data to LL
-	public void add(int data) {
+	public void addLast(int data) {
 		
 		Node newNode = new Node(data);
 		
 		if(head == null) {
 			
 			head = newNode;
+			tail = newNode;
 		}else {
-			Node currNode = head;
+			tail.next = newNode; // setting current tail pointer to new node 
+			tail = newNode;     // updating tail of the LL
+		}
+	}
+	
+	public void addFirst(int data) {
+		
+		Node newNode = new Node(data);
+		
+		if(head == null) {
+			head = newNode;
+			tail = newNode;
+		}else {
 			
-			while(currNode.next != null) {
-				currNode = currNode.next;
-			}
-			
-			currNode.next = newNode;
+			newNode.next = head;
+			head = newNode;
 		}
 	}
 	
@@ -48,16 +60,39 @@ public class LinkedListImpl {
 		
 	}
 	
+	public static void sizeLL() {
+		Node temp = head;
+		int count = 0;
+		while(temp != null) {
+			
+			count++;
+			temp = temp.next;
+		}
+		
+		System.out.println("Size of Linked List is : " + count);
+	}
+	
 	public static void main(String[] args) {
 		
 		LinkedListImpl ll = new LinkedListImpl();
 		
-		ll.add(1);
-		ll.add(2);
-		ll.add(3);
-		ll.add(4);
-		ll.add(5);
+		ll.addLast(1);
+		ll.addLast(2);
+		ll.addLast(3);
+		ll.addLast(4);
+		ll.addLast(5);
+		ll.addLast(6);
 		
-		ll.displayLL();
+		ll.displayLL(); 
+		
+		ll.addFirst(0); // will add 0 at first
+		
+		System.out.println();
+		
+		ll.displayLL(); // Out : 0 -> 1 -> 2 -> 3 -> 4 -> 5 -> 6 -> 
+		
+		System.out.println();
+		
+		ll.sizeLL();
 	}
 }
