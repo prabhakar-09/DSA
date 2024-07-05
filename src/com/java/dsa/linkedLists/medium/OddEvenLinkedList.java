dbc7d7d96396ed3db04eceab59e55ceb7e54c7d6
Prabhakar <kulkarniprabhakar2000@gmail.com>
2024-07-05 21:01:59 +0530

    Odd even solved in one traversal

--- src/com/java/dsa/linkedLists/medium/OddEvenLinkedList.java
+++ src/com/java/dsa/linkedLists/medium/OddEvenLinkedList.java
@@ -15,7 +15,51 @@ public class OddEvenLinkedList {
 		 * complexity and O(n) time complexity.
 		 */
 		
+		/**
+		 * Definition for singly-linked list.
+		 * public class ListNode {
+		 *     int val;
+		 *     ListNode next;
+		 *     ListNode() {}
+		 *     ListNode(int val) { this.val = val; }
+		 *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+		 * }
+		 */
 		
+//		
+//		class Solution {
+//		    public ListNode oddEvenList(ListNode head) {
+//
+//		        // Check for edge cases where no rearrangement is needed
+//		        if (head == null || head.next == null || head.next.next == null) {
+//		            return head;
+//		        }
+//
+//		        // Initialize pointers for odd and even lists
+//		        ListNode oddHead = head;
+//		        ListNode evenHead = head.next;
+//		        ListNode odd = oddHead;
+//		        ListNode even = evenHead;
+//
+//		        // Traverse the list and rearrange nodes
+//		        while (even != null && even.next != null) {
+//		            // Connect odd-indexed nodes
+//		            odd.next = even.next;
+//		            odd = odd.next;
+//		            
+//		            // Connect even-indexed nodes
+//		            even.next = odd.next;
+//		            even = even.next;
+//		        }
+//
+//		        // Connect the end of odd list to the beginning of even list
+//		        odd.next = evenHead;
+//
+//		        // Return the head of the rearranged list
+//		        return oddHead;
+//		    }
+//		}
+
 
 	}
 }
